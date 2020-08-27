5e00efe6c3ea4b407f952600dd2ed5d62afb3c78
Anders Strömberg <coltlaboy@yahoo.se>
2020-08-27 22:55:32 +0200

    Added params with id to PUT and DELETE endpoints

--- src/index.js
+++ src/index.js
@@ -7,22 +7,26 @@ const app = express();
 app.use(cors());
 const port = process.env.PORT;
 
+
+
 // Routes
-app.get('/', (req, res) => {
-	res.send('Recived a GET HTTP method');
-})
-
-app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+app.get('/users', (req, res) => {
+  return res.send('GET HTTP method on user resource');
 });
 
-app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+app.post('/users', (req, res) => {
+  return res.send('POST HTTP method on user resource');
 });
 
-app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+app.put('/users/:userId', (req, res) => {
+  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
 });
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+});
+
+
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
