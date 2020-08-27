46a48a162766bacc2a948d755bbc600d86dc74d4
Anders Strömberg <coltlaboy@yahoo.se>
2020-08-27 22:44:30 +0200

    Update full CRUD with mock replies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import 'dotenv/config';
 import cors from 'cors'
 import express from 'express';
+import path from 'path';
 
 const app = express();
 app.use(cors());
 const port = process.env.PORT;
 
-
 // Routes
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send('Recived a GET HTTP method');
 })
 
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
