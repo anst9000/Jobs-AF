105505d6ea47f82be070854f04173769c7a7e875
Anders Strömberg <coltlaboy@yahoo.se>
2020-08-28 10:56:54 +0200

    Refactor code and put routes in dir

---
+++ src/models/index.js
@@ -0,0 +1,33 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'Andy Backman',
+  },
+  2: {
+		id: '2',
+    username: 'Dave Davids',
+  },
+	3: {
+		id: '3',
+		username: 'Robin Wieruch',
+	},
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+
+export default {
+	users,
+	messages,
+};
